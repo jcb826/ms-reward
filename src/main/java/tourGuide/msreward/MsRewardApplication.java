2304c69ca10df756b3430c831bfc2744627203cf
jc <SM@shed31>
2021-07-20 11:28:44 +0200

    first commit

---
+++ src/main/java/tourGuide/msreward/MsRewardApplication.java
@@ -0,0 +1,13 @@
+package tourGuide.msreward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRewardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tourGuide/msreward/MsRewardApplicationTests.java
@@ -0,0 +1,13 @@
+package tourGuide.msreward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRewardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
